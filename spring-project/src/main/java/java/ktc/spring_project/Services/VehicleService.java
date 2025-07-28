da43f5941366e4f3014ea0c88af49aed45bfa96d
Xhuy09 <xhuynguyen2415@gmail.com>
2025-07-28 14:06:21 +0700

    Add Services

---
+++ spring-project/src/main/java/java/ktc/spring_project/Services/VehicleService.java
@@ -0,0 +1,36 @@
+import ktc.spring_project.entities.Vehicle;
+import ktc.spring_project.repositories.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// CRUD thông tin xe
+
+@Service
+public class VehicleService {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    public Optional<Vehicle> getVehicleById(Long id) {
+        return vehicleRepository.findById(id);
+    }
+
+    public Vehicle createVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    public Vehicle updateVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    public void deleteVehicle(Long id) {
+        vehicleRepository.deleteById(id);
+    }
+}
