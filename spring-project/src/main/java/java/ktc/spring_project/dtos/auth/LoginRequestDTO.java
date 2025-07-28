ca513dc92d4d6f60c57974491073614da52c478d
datblue21 <92386475+datblue21@users.noreply.github.com>
2025-07-28 15:16:49 +0700

    add Dtos

---
+++ spring-project/src/main/java/java/ktc/spring_project/dtos/auth/LoginRequestDTO.java
@@ -0,0 +1,36 @@
+package java.ktc.spring_project.dtos.auth;
+
+import jakarta.validation.constraints.*;
+
+/**
+ * DTO for login requests
+ */
+public class LoginRequestDTO {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    private String password;
+    
+    private boolean rememberMe = false;
+    
+    // Constructors
+    public LoginRequestDTO() {}
+    
+    public LoginRequestDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    
+    // Getters and Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    
+    public boolean isRememberMe() { return rememberMe; }
+    public void setRememberMe(boolean rememberMe) { this.rememberMe = rememberMe; }
+}
