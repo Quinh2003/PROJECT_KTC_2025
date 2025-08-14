108d1fcf7b6594ce40baf314081baba9e8825348
Fang Lee <phanhung18082003@gmail.com>
2025-08-14 21:32:08 +0700

    Fix setup

---
+++ driver-app/lib/domain/bloc/blocs.dart
@@ -0,0 +1,11 @@
+export 'package:ktc_logistics_driver/domain/bloc/auth/auth_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/cart/cart_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/delivery/delivery_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/general/general_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/map_client/mapclient_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/map_delivery/mapdelivery_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/orders/orders_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/payments/payments_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/products/products_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/user/user_bloc.dart';
+export 'package:ktc_logistics_driver/domain/bloc/my_location/mylocationmap_bloc.dart';
